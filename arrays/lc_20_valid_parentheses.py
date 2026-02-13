a9b9ee62cd3fe633f4229393cdb4df73bd39d7a8
Riddhi Shalya <riddhishalya2506@gmail.com>
2026-02-13 22:41:36 +0530

    Add LC 20 Valid Parentheses

---
+++ arrays/lc_20_valid_parentheses.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isValid(self, s):
+        stack = []
+        mapping = {')': '(', '}': '{', ']': '['}
+
+        for ch in s:
+            if ch in mapping:
+                if not stack or stack[-1]!=mapping[ch]:
+                    return False
+                stack.pop()
+                
+            else:
+                stack.append(char)
+
+        return not stack
